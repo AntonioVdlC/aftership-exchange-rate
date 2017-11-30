bd01afd1adbc5e2a6c0e81ac333a8f3027b5572d
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2017-11-30 14:19:19 +0800

    Refactor client code

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Main from './components/Main'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+        <Header>AfterShip Exchange Rate</Header>
+        <Main>
+          <p className="intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </Main>
+        <Footer>Made on a plane ✈ by Antonio Villagra De La Cruz</Footer>
       </div>
     );
   }
